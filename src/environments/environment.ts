8489020eaadd3a02654f4e6437aa1bf83cafcafc
jbloup <jbloup@hotmail.fr>
2022-05-05 13:05:12 +0200

    fix error cors, virer caddy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   description: 'La plateforme pour améliorer vos relations',
   version: '1.0.0',
 
-  apiUrl: 'http://localhost:82',
+  apiUrl: 'http://localhost:8000',
 };
 
 /*
