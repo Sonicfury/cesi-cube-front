f289d905f84b1c8fa90ab26802edb00138a2458c
JohnnyBoy <theo.lambrt@gmail.com>
2022-05-30 17:18:54 +0200

    fix(nav): "Mon profil" was showing even after logout
    - added watcher on SessionState in nav component

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,6 +3,8 @@ import {SessionService} from "../../services/session.service";
 import {environment} from "../../../environments/environment";
 import {BaseComponent} from "../base-component";
 import {AuthorizationService} from "../../services/authorization.service";
+import {User} from "../../models/user";
+import {SessionState} from "../../services/session-state";
 
 @Component({
   selector: 'app-nav',
@@ -14,9 +16,11 @@ export class NavComponent extends BaseComponent implements OnInit {
   headerLinkTitle: string = `Accueil - ${environment.appName}`;
   appName: string = environment.appName;
   appDescription: string = environment.description;
+  isConnected: boolean = false;
 
   constructor(private _authorizationService: AuthorizationService, private _sessionService: SessionService) {
     super('Navigation', _authorizationService);
+    this.sessionService.watch((data: { state: SessionState, user?: User }) => this.isConnected = data.state === SessionState.CONNECTED)
   }
 
   ngOnInit(): void {
