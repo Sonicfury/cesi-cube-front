6f3d47df479fcfe0e8a7f26371a855b4ea558821
johnnyboy <theo.lambert@viacesi.fr>
2022-07-06 02:03:02 +0200

    feat(filters): improved laravel filters helper

--- src/app/helpers/filters.helper.ts
+++ src/app/helpers/filters.helper.ts
@@ -1,93 +1,105 @@
 export class LaravelQueryBuilder {
-  public filters: LaravelQueryFilter[][] = [[]]
-  public sorts: LaravelQuerySort[] = []
-  public page: LaravelQueryPage = new LaravelQueryPage(0, 10)
-  public sums: Set<string> = new Set<string>()
-  public withs: Set<string> = new Set<string>()
+  private _filters: LaravelQueryFilter[][] = [[]]
+  private _sorts: LaravelQuerySort[] = []
+  private _page: LaravelQueryPage = new LaravelQueryPage(0, 10)
+  private _sums: Set<string> = new Set<string>()
+  private _withs: Set<string> = new Set<string>()
 
   constructor() {
   }
 
+  public query(): string {
+    const query = JSON.stringify({
+      page: this._page,
+      sorts: this._sorts,
+      filters: this._filters,
+      sums: Array.from(this._sums),
+      withs: Array.from(this._withs)
+    })
+
+    return `q=${query}`
+  }
+
   public addFilter(field: string,
                    op: '=' | '!=' | '<>' | '>' | '>=' | '<' | '<=' | 'like' | 'not like' | 'in' | 'not' | 'is',
-                   value: string,
+                   value: string|number|boolean,
                    has?: boolean): LaravelQueryBuilder {
     const filter = has ? new LaravelQueryFilter(field, op, value, has) : new LaravelQueryFilter(field, op, value)
-    this.filters[0].push(filter)
+    this._filters[0].push(filter)
 
     return this
   }
 
   public removeFilter(field: string): LaravelQueryBuilder {
-    this.filters[0] = this.filters[0].filter(f => f.field !== field)
+    this._filters[0] = this._filters[0].filter(f => f.field !== field)
 
     return this
   }
 
   public addSort(field: string, dir: 'asc' | 'desc'): LaravelQueryBuilder {
-    const exists = this.sorts.find(s => s.field === field)
+    const exists = this._sorts.find(s => s.field === field)
     if (exists) {
       exists.dir = dir
 
       return this
     }
     const sort = new LaravelQuerySort(field, dir)
-    this.sorts.push(sort)
+    this._sorts.push(sort)
 
     return this
   }
 
   public removeSort(field: string) {
-    this.sorts = this.sorts.filter(s => s.field !== field)
+    this._sorts = this._sorts.filter(s => s.field !== field)
 
     return this
   }
 
   public clearSorts() {
-    this.sorts = []
+    this._sorts = []
 
     return this
   }
 
   public pageOffset(offset: number): LaravelQueryBuilder {
-    this.page.offset = offset
+    this._page.offset = offset
 
     return this
   }
 
   public pageLimit(limit: number): LaravelQueryBuilder {
-    this.page.limit = limit
+    this._page.limit = limit
 
     return this
   }
 
   public clearPage(limit?: number): LaravelQueryBuilder {
-    this.page.offset = 0
-    if (limit) this.page.limit = limit
+    this._page.offset = 0
+    if (limit) this._page.limit = limit
 
     return this
   }
 
   public addSum(sum: string): LaravelQueryBuilder {
-    this.sums.add(sum)
+    this._sums.add(sum)
 
     return this
   }
 
   public removeSum(sum: string): LaravelQueryBuilder {
-    this.sums.delete(sum)
+    this._sums.delete(sum)
 
     return this
   }
 
   public addWith(wiz: string): LaravelQueryBuilder {
-    this.withs.add(wiz)
+    this._withs.add(wiz)
 
     return this
   }
 
   public removeWith(wiz: string): LaravelQueryBuilder {
-    this.withs.delete(wiz)
+    this._withs.delete(wiz)
 
     return this
   }
@@ -96,7 +108,7 @@ export class LaravelQueryBuilder {
 export class LaravelQueryFilter {
   constructor(public field: string,
               public op: '=' | '!=' | '<>' | '>' | '>=' | '<' | '<=' | 'like' | 'not like' | 'in' | 'not' | 'is',
-              public value: string,
+              public value: string|number|boolean,
               public has?: boolean) {
   }
 }
